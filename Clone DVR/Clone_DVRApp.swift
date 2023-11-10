8c7e44d8e2737d2e10e3d59887020404ad7cfe44
Daniel <dan@Daniels-MacBook-Pro.local>
2023-11-10 09:26:12 +0700

    init

---
+++ Clone DVR/Clone_DVRApp.swift
@@ -0,0 +1,17 @@
+//
+//  Clone_DVRApp.swift
+//  Clone DVR
+//
+//  Created by Daniel on 10/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct Clone_DVRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
